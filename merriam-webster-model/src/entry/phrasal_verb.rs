993c34ca5e0045dec8eb6ccdf51dbea94df93726
Joshua Taylor <sonicbphuct@gmail.com>
2025-03-08 10:22:54 +0100

    finally fixed

--- merriam-webster-model/src/entry/phrasal_verb.rs
+++ merriam-webster-model/src/entry/phrasal_verb.rs
@@ -1,31 +1,24 @@
-
 #[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
 pub enum PhrasalVerbs {
     Sphrasev(SPhrasalVerb),
-    Phrasev(PhrasalVerb)
+    Phrasev(PhrasalVerb),
 }
 
+pub type PhrasalVerb = Vec<InnerPhrasalVerb>;
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct InnerPhrasalVerb{
+pub struct InnerPhrasalVerb {
     /// phrasal verb (required)
     #[serde(rename = "pva")]
     pub phrasal_verb: String,
-    #[serde(rename = "pvl")]
+    #[serde(rename = "pvl", default)]
     pub phrasal_verb_label: Option<String>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(tag = "sphrasev")]
-pub struct SPhrasalVerb{
+pub struct SPhrasalVerb {
     pub phrs: Vec<InnerPhrasalVerb>,
-    #[serde(rename = "phsls")]
-    pub phrasal_verb_subject_label: Vec<String>,
+    #[serde(rename = "phsls", default)]
+    pub phrasal_verb_subject_label: Option<Vec<String>>,
 }
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(tag = "phrasev")]
-pub struct PhrasalVerb {
-    pub phr: InnerPhrasalVerb,
-}
-
-
