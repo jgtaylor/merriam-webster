c5c19741063ffc111eb23a4fd34fd74c827a8169
Joshua Taylor <sonicbphuct@gmail.com>
2025-03-14 15:52:21 +0100

    add copy trait, add trait GetInner

--- merriam-webster-model/src/entry/sense/binding_substitute.rs
+++ merriam-webster-model/src/entry/sense/binding_substitute.rs
@@ -7,8 +7,21 @@ pub struct InnerBindingSubstitute {
     pub sense: SenseObject,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub enum BindingSubstituteKey {
     #[serde(rename = "bs")]
     Key,
+}
+
+pub trait GetInner {
+    fn get_inner(&self) -> Vec<&SenseObject>;
+}
+
+impl GetInner for BindingSubstitute {
+    fn get_inner(&self) -> Vec<&SenseObject> {
+        let mut return_senses = vec![];
+        let (key, object) = &*self;
+        return_senses.push(&object.sense);
+        return_senses
+    }
 }
